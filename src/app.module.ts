2cc7152837c18f852040f5d47cafc4c09c3b4321
2382003n <2382003@unai.edu>
2025-05-03 14:51:59 +0700

    minor bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,8 +11,7 @@ import { UserModule } from './user/user.module';
 import { PembeliModule } from './pembeli/pembeli.module';
 import { PegawaiModule } from './pegawai/pegawai.module';
 import { LayananModule } from './layanan/layanan.module';
-import { PostModule } from './post/post.module';
-import { CommentModule } from './comment/comment.module';
+
 
 @Module({
   imports: [
@@ -39,9 +38,7 @@ import { CommentModule } from './comment/comment.module';
     UserModule,
     PembeliModule, // Modul Pembeli
     PegawaiModule, // Modul Pegawai
-    LayananModule, // Modul Layanan
-    PostModule,
-    CommentModule,
+    LayananModule
   ],
   controllers: [AppController],
   providers: [
